672056d6d9d2f656f68dcf160e8785f676aae26b
aihara44 <amirhasan.zaizakarani@gmail.com>
2018-09-29 01:02:57 +0800

    k

---
+++ includes/wrapper.php
@@ -0,0 +1,43 @@
+    <div id="wrapper">
+
+      <!-- Sidebar -->
+      <ul class="sidebar navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="dashboard.php">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span>
+          </a>
+        </li>
+      
+       
+        <li class="nav-item active">
+          <a class="nav-link" href="tables.html">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Tables</span></a>
+        </li>
+      </ul>
+
+      <div id="content-wrapper">
+
+        <div class="container-fluid">
+
+          <!-- Breadcrumbs-->
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+              <a href="#">Dashboard</a>
+            </li>
+            <li class="breadcrumb-item active">Tables</li>
+          </ol>
+
+       
+
+        </div>
+        <!-- /.container-fluid -->
+
+    
+    
+
+      </div>
+      <!-- /.content-wrapper -->
+
+    </div>
